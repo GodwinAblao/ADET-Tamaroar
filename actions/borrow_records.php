3055893e19a6d729b53f954bb6989b7425e4e4d1
GodwinAblao <wengdrich@gmail.com>
2025-07-09 17:05:46 +0800

    feat(library_system.sql): enchanced

--- actions/borrow_records.php
+++
@@ -1,3 +0,0 @@
-<div style="padding:2rem; color:#ffdd57; font-weight:700;">Borrow Records section is under construction.</div>
-
-
--- actions/edit_book.php
+++
@@ -1,3 +0,0 @@
-<div style="padding:2rem; color:#ffdd57; font-weight:700;">Edit Book section is under construction.</div>
-
-
